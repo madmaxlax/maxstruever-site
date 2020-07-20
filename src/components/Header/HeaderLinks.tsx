c5b552a0ea61ed5a50b7b10cd547f54e75bc5ec0
Max Struever <maxstruever@gmail.com>
2020-07-20 16:30:39 -0400

    fix(links): download resume

--- src/components/Header/HeaderLinks.tsx
+++ src/components/Header/HeaderLinks.tsx
@@ -1,13 +1,13 @@
-import { Apps } from "@material-ui/icons";
-import Button from "../CustomButtons/ButtonLink";
-import CustomDropdown from "../CustomDropdown/CustomDropdown";
-import { Link } from "react-router-dom";
-import List from "@material-ui/core/List";
-import ListItem from "@material-ui/core/ListItem";
-import React from "react";
-import Tooltip from "@material-ui/core/Tooltip";
-import headerLinksStyle from "../../assets/jss/material-kit-react/components/headerLinksStyle";
-import withStyles from "@material-ui/core/styles/withStyles";
+import { Apps } from '@material-ui/icons';
+import Button from '../CustomButtons/ButtonLink';
+import CustomDropdown from '../CustomDropdown/CustomDropdown';
+import { Link } from 'react-router-dom';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import React from 'react';
+import Tooltip from '@material-ui/core/Tooltip';
+import headerLinksStyle from '../../assets/jss/material-kit-react/components/headerLinksStyle';
+import withStyles from '@material-ui/core/styles/withStyles';
 
 function HeaderLinks({ ...props }) {
   const { classes } = props;
@@ -19,13 +19,16 @@ function HeaderLinks({ ...props }) {
           buttonText="Menu"
           buttonProps={{
             className: classes.navLink,
-            color: "transparent"
+            color: 'transparent',
           }}
           buttonIcon={Apps}
           dropdownList={[
             <Link to="/" className={classes.dropdownLink}>
               Home
             </Link>,
+            <a href="http://resume.maxstruever.com/" className={classes.dropdownLink}>
+              Download Resume
+            </a>,
             <a href="http://portfolio.maxstruever.com/" className={classes.dropdownLink}>
               Portfolio
             </a>,
@@ -34,29 +37,27 @@ function HeaderLinks({ ...props }) {
             </Link>,
             <Link to="/referrals" className={classes.dropdownLink}>
               Referral Links
-            </Link>
+            </Link>,
           ]}
         />
       </ListItem>
 
-      <ListItem className={classes.listItem}>
-
-       
-      </ListItem>
+      <ListItem className={classes.listItem}></ListItem>
       <ListItem className={classes.listItem}>
         <Tooltip
           id="instagram-facebook"
           title="My Github"
-          placement={window.innerWidth > 959 ? "top" : "left"}
+          placement={window.innerWidth > 959 ? 'top' : 'left'}
           classes={{ tooltip: classes.tooltip }}
         >
           <Button
             color="transparent"
             href="https://github.com/madmaxlax"
-            target="_blank" rel="noopener noreferrer"
+            target="_blank"
+            rel="noopener noreferrer"
             className={classes.navLink}
           >
-            <i className={classes.socialIcons + " fab fa-github"} />
+            <i className={classes.socialIcons + ' fab fa-github'} />
           </Button>
         </Tooltip>
       </ListItem>
@@ -64,16 +65,17 @@ function HeaderLinks({ ...props }) {
         <Tooltip
           id="instagram-tooltip"
           title="Follow me on instagram"
-          placement={window.innerWidth > 959 ? "top" : "left"}
+          placement={window.innerWidth > 959 ? 'top' : 'left'}
           classes={{ tooltip: classes.tooltip }}
         >
           <Button
             color="transparent"
             href="https://www.instagram.com/madmaxlax"
-            target="_blank" rel="noopener noreferrer"
+            target="_blank"
+            rel="noopener noreferrer"
             className={classes.navLink}
           >
-            <i className={classes.socialIcons + " fab fa-instagram"} />
+            <i className={classes.socialIcons + ' fab fa-instagram'} />
           </Button>
         </Tooltip>
       </ListItem>
