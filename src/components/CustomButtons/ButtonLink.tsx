e395f38506dacf359af9b35fd9b5a9aa6cb060aa
Max Struever <struever.max@bcgplatinion.com>
2022-04-14 13:53:40 -0500

    disable prettier

--- src/components/CustomButtons/ButtonLink.tsx
+++ src/components/CustomButtons/ButtonLink.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 import { Button } from '@material-ui/core';
 import { default as MaterialUILink, LinkTypeMap } from '@material-ui/core/Link';
 import { makeStyles } from '@material-ui/core/styles';
@@ -11,7 +12,6 @@ import {
   primaryColor,
   roseColor,
   successColor,
-  // eslint-disable-next-line prettier/prettier
   warningColor
 } from '../../assets/jss/material-kit-react';
 
